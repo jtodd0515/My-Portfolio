2c8863bebc7420f2ba349f0134c5b6cd05a0562f
James Todd <jtodd0515@gmail.com>
2020-07-18 23:43:08 -0400

    moved folders into components group

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,6 +4,8 @@ import { Button } from 'react-bootstrap';
 
 import { Card } from 'react-bootstrap';
 
+
+
 import './About.css';
 
 
@@ -15,13 +17,15 @@ export default class MeCard extends Component {
         return (
 
         <Card className="card container" bg="transparent" style={{ width: '50rem' }}>
-            <Card.Img className="card-img" variant="left" src="./images/Headshot.jpg/100px180" />
-            <Card.Body>
-            <Button className="button" variant="primary" href="./Resume/Resume-pdf.pdf">Resume</Button>
-                <Card.Text className="card-text">
-                    I'm a Software Engineer from Raleigh, NC. I have a professional background in Automotive Systems Technology, which like coding utilizes my knowledge of diagnostics and hands on ability to build or repair parts of the system.
+            
+            <Card.Img className="card-img" variant="left" src='./Components/Images/Headshots.jpg' alt="Profile Headshot" />
+            <Button className="button" variant="primary" href="./Components/Resume/Resume-pdf.pdf">Resume</Button>
+                <Card.Body>
+                    <Card.Text className="edge scroll">
+                    I'm a Software Engineer from Raleigh, NC. I have a professional background in Automotive Systems Technology, which like coding utilizes my knowledge of diagnostics and hands on ability to build and repair systems. Through my work in the automotive industry, it has enabled me to adapt to changing technology as well as challenged my problem solving skills ensuring in am at the top of my game at all times. The same problem solving skills have become exceptionally helpful in my progression into the coding and software engineering field. I am pleased to be able to apply the same mechanical and diagnositic techniques to software engineering that have more then proven their worth in the automotive world.
                 </Card.Text>
             </Card.Body>
+            
         </Card>
         )
     }
