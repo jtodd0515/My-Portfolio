834a147d3191fc1ff2f834809e4f68f1336de259
Karsten Rabe <karstenrabe91@gmail.com>
2020-07-20 09:10:38 -0400

    feat(cleanup): organize dirs & readability tweaks

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,29 +4,28 @@ import { Button } from 'react-bootstrap';
 
 import { Card } from 'react-bootstrap';
 
-import Headshot from './Images/Headshot.jpg';
+import Headshot from '../../images/Headshot.jpg';
 
 import './About.css';
 
-
-
-
 export default class MeCard extends Component {
 
-    render() {
-        return (
+  render() {
+    return (
+      <div id='about'>
 
         <Card className="card2 container" bg="transparent" style={{ width: '50rem' }}>
-            
-            <Card.Img className="card-img" variant="left" src={Headshot} alt="Profile Headshot" />
-            <Button className="button" variant="primary" href="./Resume/Resume-pdf.pdf">Resume</Button>
-                <Card.Body>
-                    <Card.Text className="edge scroll">
-                    I'm a Software Engineer from Raleigh, NC. I have a professional background in Automotive Systems Technology, which like coding utilizes my knowledge of diagnostics and hands on ability to build and repair systems. Through my work in the automotive industry, it has enabled me to adapt to changing technology as well as challenged my problem solving skills ensuring in am at the top of my game at all times. The same problem solving skills have become exceptionally helpful in my progression into the coding and software engineering field. I am pleased to be able to apply the same mechanical and diagnositic techniques to software engineering that have more then proven their worth in the automotive world.
-                </Card.Text>
-            </Card.Body>
-            
+
+          <Card.Img className="card-img" variant="left" src={Headshot} alt="Profile Headshot" />
+          <Button className="button" variant="primary" href="/Resume.pdf" target='_blank'>Resume</Button>
+          <Card.Body>
+            <Card.Text className="edge scroll">
+              I'm a Software Engineer from Raleigh, NC. I have a professional background in Automotive Systems Technology, which like coding utilizes my knowledge of diagnostics and hands on ability to build and repair systems. Through my work in the automotive industry, it has enabled me to adapt to changing technology as well as challenged my problem solving skills ensuring in am at the top of my game at all times. The same problem solving skills have become exceptionally helpful in my progression into the coding and software engineering field. I am pleased to be able to apply the same mechanical and diagnositic techniques to software engineering that have more then proven their worth in the automotive world.
+            </Card.Text>
+          </Card.Body>
+
         </Card>
-        )
-    }
+      </div>
+    )
+  }
 }
--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,20 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import { Jumbotron } from 'react-bootstrap';
 
 import './Project.css';
 
 export default class Fluid extends Component {
-    
-    render(){
-        return (
-            <Jumbotron fluid className="jumbotron">
-            
-            <h1>Projects</h1>
-            <p>
-              These are the projects I have created using various languages.
+
+  render() {
+    return (
+      <Jumbotron fluid className="jumbotron" id='projects'>
+
+        <h1>Projects</h1>
+        <p>
+          These are the projects I have created using various languages.
             </p>
-            </Jumbotron>
-        )
-    }
+      </Jumbotron>
+    )
+  }
 }
