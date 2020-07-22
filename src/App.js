ee64b956b9927fd35231d5b6a940c4d2a7c123e1
James Todd <jtodd0515@gmail.com>
2020-07-22 17:59:58 -0400

    changes to local file before merge

--- src/App.js
+++ src/App.js
@@ -22,16 +22,16 @@ function App() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="Portfolio.html">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href='#'>Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="about.html">About</a>
+              <a className="nav-link" href='#About'>About</a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="<Jumbotron />">Projects</a>
+              <a className="nav-link" href='#Jumbotron'>Projects</a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="contact.html">Contact</a>
+              <a className="nav-link" href='#Contact'>Contact</a>
             </li>
           </ul>
         </div>
